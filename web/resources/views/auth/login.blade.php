bc24339563b86f9a51ccadf2bfc16da14ff74227
Samuel Sihotang <samsihotang1@gmail.com>
2024-07-08 09:51:06 +0700

    add login page

--- web/resources/views/auth/login.blade.php
+++ web/resources/views/auth/login.blade.php
@@ -1,34 +1,33 @@
-<form method="POST" action="{{ url('/login') }}">
-    @csrf
-
-    <div>
-        <label for="username">Username</label>
-        <input id="username" type="text" name="username" value="{{ old('username') }}" required autocomplete="username"
-            autofocus>
-        @error('username')
-        <span role="alert">{{ $message }}</span>
-        @enderror
-    </div>
-
-    <div>
-        <label for="password">Password</label>
-        <input id="password" type="password" name="password" required autocomplete="current-password">
-        @error('password')
-        <span role="alert">{{ $message }}</span>
-        @enderror
-    </div>
-
-    <div>
-        <label for="password_confirmation">Konfirmasi Password</label>
-        <input id="password_confirmation" type="password" name="password_confirmation" required autocomplete="current-password">
-        @error('password')
-        <span role="alert">{{ $message }}</span>
-        @enderror
-    </div>
-
-    <div>
-        <button type="submit">
-            Login
-        </button>
-    </div>
-</form>
+<div>
+    <main class="auth-minimal-wrapper">
+        <div class="auth-minimal-inner">
+            <div class="minimal-card-wrapper">
+                <div class="card mb-4 mt-5 mx-4 mx-sm-0 position-relative">
+                    <div class="card-body p-sm-5">
+                        <h2 class="fs-20 fw-bolder mb-4">Login</h2>
+                        <form wire:submit.prevent="login" class="w-100 mt-4 pt-2">
+                            @csrf
+                            <div class="mb-4">
+                                <input type="text" wire:model="nip" class="form-control" placeholder="NIP" required
+                                    autocomplete="nip">
+                                @error('nip')
+                                <span role="alert">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <input type="password" wire:model="password" class="form-control" placeholder="Password"
+                                    autocomplete="current-password" required>
+                                @error('password')
+                                <span role="alert">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="mt-5">
+                                <button type="submit" class="btn btn-lg btn-primary w-100">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</div>
--- web/routes/web.php
+++ web/routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Livewire\Login;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Counter;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', Login::class)->name('login');
+Route::post('/logout', [Login::class , 'logout'])->name('logout');
 
-Route::get('/login', [AuthenticationController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthenticationController::class, 'login']);
-Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/', function () {
+            return view('welcome');
+        }
+        );
+        Route::get('/counter', Counter::class);
+    });
