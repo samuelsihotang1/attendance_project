1fb6bd738ae99b9041279aa8aca3fb720540607b
Samuel Sihotang <samsihotang1@gmail.com>
2024-07-28 21:45:21 +0700

    add create employee

--- web/routes/web.php
+++ web/routes/web.php
@@ -3,6 +3,7 @@
 use App\Livewire\Dashboard\DashboardCheckIn;
 use App\Livewire\Dashboard\DashboardCheckOut;
 use App\Livewire\Auth\Login;
+use App\Livewire\Employee\Create;
 use App\Livewire\Employee\Employee;
 use Illuminate\Support\Facades\Route;
 
@@ -12,9 +13,10 @@ Route::middleware('guest')->group(function () {
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [Login::class, 'logout'])->name('logout');
-    Route::get('/', DashboardCheckIn::class);
-    Route::get('/out', DashboardCheckOut::class);
-    Route::get('/employee', Employee::class);
+    Route::get('/', DashboardCheckIn::class)->name('checkin');
+    Route::get('/out', DashboardCheckOut::class)->name('checkout');
+    Route::get('/employee', Employee::class)->name('employee');
+    Route::get('/employee/create', Create::class)->name('employee.create');
 });
 
 // Route::get('/test', [DashboardCheckIn::class, 'getUsers']);
