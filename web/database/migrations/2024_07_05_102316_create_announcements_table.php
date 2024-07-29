aa5d696889d7ae91dfdf28ad6f18603812a65655
Samuel Sihotang <samsihotang1@gmail.com>
2024-07-29 20:28:15 +0700

    change atribut of announcement

--- web/database/migrations/2024_07_05_102316_create_announcements_table.php
+++ web/database/migrations/2024_07_05_102316_create_announcements_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,6 +13,7 @@ return new class extends Migration
         Schema::create('announcements', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title');
+            $table->string('slug')->unique();
             $table->text('content');
             $table->timestamps();
         });
--- web/database/seeders/AnnouncementSeeder.php
+++ web/database/seeders/AnnouncementSeeder.php
@@ -15,26 +15,31 @@ class AnnouncementSeeder extends Seeder
     {
         Announcement::create([
             'title' => 'Office Closure',
+            'slug' => 'office-closure',
             'content' => 'The office will be closed on July 4th for Independence Day.',
         ]);
 
         Announcement::create([
             'title' => 'New Policy Update',
+            'slug' => 'new-policy-update',
             'content' => 'Please review the new company policies updated on the HR portal.',
         ]);
 
         Announcement::create([
             'title' => 'Team Meeting',
+            'slug' => 'team-meeting',
             'content' => 'There will be a team meeting on Friday at 10 AM in the conference room.',
         ]);
 
         Announcement::create([
             'title' => 'Holiday Schedule',
+            'slug' => 'holiday-schedule',
             'content' => 'The holiday schedule for the upcoming year has been posted on the intranet.',
         ]);
 
         Announcement::create([
             'title' => 'Office Maintenance',
+            'slug' => 'office-maintenance',
             'content' => 'Office maintenance will occur this weekend. Please ensure your desks are cleared.',
         ]);
     }
