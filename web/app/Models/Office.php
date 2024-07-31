1f9ed32225f22b212f7284dba6c26a8c2423d336
Samuel Sihotang <samsihotang1@gmail.com>
2024-07-31 14:04:28 +0700

    add slug on office

--- web/app/Models/Office.php
+++ web/app/Models/Office.php
@@ -13,6 +13,7 @@ class Office extends Model
 
     protected $fillable = [
         'name',
+        'slug',
         'address',
         'latitude',
         'longitude',
--- web/database/migrations/0001_01_01_000000_create_offices_table.php
+++ web/database/migrations/0001_01_01_000000_create_offices_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
         Schema::create('offices', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug')->unique();
             $table->string('address');
             $table->string('latitude');
             $table->string('longitude');
